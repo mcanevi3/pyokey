d3052620a696dc94de539e12c432a306a6423081
Mehmet Canevi <m.canevi3@gmail.com>
2025-03-26 15:41:51 +0300

    cont'd

--- Core/Console.py
+++ Core/Console.py
@@ -2,14 +2,14 @@ from colorama import Fore, Style
 
 def print_color(string:str,color:str):
     if color=="r":
-        print(Fore.RED + string + Style.RESET_ALL)
+        print(Fore.RED + string + Style.RESET_ALL, end=" ")
     elif color=="g":
-        print(Fore.GREEN + string + Style.RESET_ALL)
+        print(Fore.GREEN + string + Style.RESET_ALL, end=" ")
     elif color=="b":
-        print(Fore.BLUE + string + Style.RESET_ALL)
+        print(Fore.BLUE + string + Style.RESET_ALL, end=" ")
     elif color=="k":
-        print(Fore.BLACK + string + Style.RESET_ALL)
+        print(Fore.BLACK + string + Style.RESET_ALL, end=" ")
     elif color=="y":
-        print(Fore.YELLOW + string + Style.RESET_ALL)
+        print(Fore.YELLOW + string + Style.RESET_ALL, end=" ")
     else:    
-        print(string + Style.RESET_ALL)
+        print(string + Style.RESET_ALL, end=" ")
--- Core/OkeyTile.py
+++ Core/OkeyTile.py
@@ -4,22 +4,22 @@ import Console
 import OkeyMath
 
 class OkeyValue(IntEnum):
-    TFalseOkey=0
-    T1=1
-    T2=2
-    T3=3
-    T4=4
-    T5=5
-    T6=6
-    T7=7
-    T8=8
-    T9=9
-    T10=10
-    T11=11
-    T12=12
-    T13=13
+    T1=0
+    T2=1
+    T3=2
+    T4=3
+    T5=4
+    T6=5
+    T7=6
+    T8=7
+    T9=8
+    T10=9
+    T11=10
+    T12=11
+    T13=12
+    
     def to_int(self):
-        return self.value
+        return self.value+1
 
 class OkeyColor(IntEnum):
     RED=0
@@ -28,27 +28,55 @@ class OkeyColor(IntEnum):
     BLUE=3
     def to_int(self):
         return self.value
+    
+class OkeyType(IntEnum):
+    STANDART=0
+    FALSE_OKEY=1
+    OKEY=2
+    def to_int(self):
+        return self.value
 
 class OkeyTile():
     Value:OkeyValue
     Color:OkeyColor
+    Type=OkeyType.STANDART
     
     def __init__(self,*args):
         if len(args)==0:
-            self.Value=OkeyValue(OkeyMath.random_int(14))
+            self.Value=OkeyValue(OkeyMath.random_int(13))
             self.Color=OkeyColor(OkeyMath.random_int(4))
         else:
             val=args[0]
-            self.Value=OkeyValue(val % 14)
-            self.Color=OkeyColor(val//14)
-        
+            self.Value=OkeyValue(val % 13)
+            self.Color=OkeyColor(val//13)
+            
+    def to_int(self):
+        val=13*self.Color.to_int()+self.Value.to_int()
+        return val
+    
     def print(self):
         colorvec=["r","y","k","b"]
         color=colorvec[self.Color.to_int()]
-        if self.Value is OkeyValue.TFalseOkey:
-            Console.print_color("FalseOkey",color)
-        else:
-            Console.print_color(f"{self.Value}",color)
 
-for i in range(14*4):
-    OkeyTile(i).print()
+        if self.Type is OkeyType.STANDART:
+            Console.print_color(f"{self.Value.to_int()}",color)
+        elif self.Type is OkeyType.FALSE_OKEY:
+            Console.print_color(f"{self.Value.to_int()} FalseOkey",color)
+        else:
+            Console.print_color(f"[{self.Value.to_int()}]",color)
+
+class OkeyStack:
+    Tiles=list(range(0,52))+list(range(0,52))
+
+    def __init__(self):
+        self.Tiles=OkeyMath.shuffle(self.Tiles)
+
+    def print(self):
+        for tile in self.Tiles:
+            OkeyTile(tile).print()
+
+    def draw_tile(self):
+        return OkeyTile(self.Tiles.pop())
+
+stack=OkeyStack()
+stack.print()
